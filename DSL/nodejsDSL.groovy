1390531e9d2f476b4235ef7e5a075b381916e3ef
LibrerosDavid <89157243+LibrerosDavid@users.noreply.github.com>
2022-10-20 13:33:36 -0500

    Update nodejsDSL.groovy

--- DSL/nodejsDSL.groovy
+++ DSL/nodejsDSL.groovy
@@ -10,7 +10,7 @@ job('Aplicacion Node.js DSL') {
         scm('H/7 * * * *')
     }
     wrappers {
-        nodejs('Nodejs')
+        Nodejs('nodejs')
     }
     steps {
         shell("npm install")
